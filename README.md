9b3b448989347a6d255bd5ca2d656b696de774de
Luis Mesas <luismesas@gmail.com>
2014-01-26 11:53:15 +0100

    Added blink example

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ A library for using Raspberry Pi on Golang
 current features
 ----------------
 
-* ioctl - syscall messages
-* spi - allows spi communication with conencted spi devices
+* ioctl - syscall ioctl needed for sending spi signals
+* spi - allows spi communication with connected spi devices
 * MCP23S17 - compatibility with Microchip MCP23S17 16-Bit I/O Expander with Serial Interface.
+
+usage
+-----
+
+See examples folder
---
+++ examples/blink/blink.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/luismesas/go-rpi/MCP23S17"
+	"github.com/luismesas/go-rpi/spi"
+	"log"
+	"time"
+)
+
+func main(){
+
+	// creates a new MCP23S17 instance
+	mcp := MCP23S17.NewMCP23S17(spi.DEFAULT_HARDWARE_ADDR, spi.DEFAULT_BUS, spi.DEFAULT_CHIP)
+
+	// GPIOa outputs
+	gpioa := make([]*MCP23S17.MCP23S17RegisterBit,8)
+	for i := range(gpioa){
+		gpioa[i] = MCP23S17.NewMCP23S17RegisterBit(uint(i), MCP23S17.GPIOA, mcp)
+	}
+
+	// Connects to chip
+	err := mcp.Open()
+	if err != nil {
+		log.Fatalf("Error connecting to chip: %s\n", err)
+	}
+
+	// Blinks output 7
+	for {
+		gpioa[7].Toggle()
+		time.Sleep(time.Second)
+	}
+}
